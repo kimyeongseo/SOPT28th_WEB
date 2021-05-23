52ceaee4c05152c30ef0281bd5d01226cebbd142
kimyeongseo <oseo0202@naver.com>
2021-05-23 23:18:00 +0900

    repos random 주소 불러오기

--- 3rd_seminar/src/components/Result.js
+++ 3rd_seminar/src/components/Result.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import RepoCard from './RepoCard';
 import ResultCard from "./ResultCard";
 
 const Result = ({ userData }) => {
-    const { status, data } = userData;
+    const { status, data, repos } = userData;
     switch (status) {
         case "pending":
             return <div style={{ color: "white", fontSize: "24px" }}>Loading...</div>;
         case "resolved":
-            return <ResultCard data={data} />;
+            return (
+                <>
+                    <ResultCard data={data} />
+                    <RepoCard repos={repos} />
+                </>);
         case "rejected":
             return (
                 <div style={{ color: "white", fontSize: "24px" }}>User Not Found</div>
