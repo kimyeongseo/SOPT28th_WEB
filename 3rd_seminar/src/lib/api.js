4fc9dcb4a4fff1ae86a3412fe3b990dfd4b62824
kimyeongseo <oseo0202@naver.com>
2021-05-23 22:48:48 +0900

    userRepoData API연결

--- 3rd_seminar/src/lib/api.js
+++ 3rd_seminar/src/lib/api.js
@@ -10,3 +10,14 @@ export const getUserData = async (id) => {
         return null;
     }
 }
+
+export const getUserRepos = async (id) => {
+    try {
+        const { data } = await axios.get(`https://api.github.com/users/${id}/repos`);
+        console.log("레포 성공", data);
+        return data;
+    } catch (e) {
+        console.log("레포 실패", e);
+        return null;
+    }
+}
